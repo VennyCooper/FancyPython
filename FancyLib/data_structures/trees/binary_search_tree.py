1cda320d69c052ae37966f1fa98ad09bb3f43f71
Wenqiang Luo <wenqiangluo1010@outlook.com>
2019-07-31 18:07:46 +0800

    20190731 local commit

--- FancyLib/data_structures/trees/binary_search_tree.py
+++ FancyLib/data_structures/trees/binary_search_tree.py
@@ -2,5 +2,21 @@ from tree import Tree
 from node import Node
 
 class BinarySearchTree(Tree):
-    def __init__(self):
+    def delete(self, data):
         pass
+    
+    def is_tree_empty(self):
+        return self.root is not None
+
+
+    def search(self, data) -> Node:
+        tmp_node = self.root
+        while tmp_node != None:
+            if data == tmp_node.data:
+                break
+            elif data < tmp_node.data:
+                tmp_node = tmp_node.left
+            else:
+                tmp_node = tmp_node.right
+        return tmp_node
+
